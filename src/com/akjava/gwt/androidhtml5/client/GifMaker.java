fa99ce06a9ce63da60156d1ab2f0402b75f14aa9
aki <aki@xps>
2014-04-11 11:01:21 +0900

    add ActionCellGenerator

--- src/com/akjava/gwt/androidhtml5/client/GifMaker.java
+++ src/com/akjava/gwt/androidhtml5/client/GifMaker.java
@@ -1,6 +1,7 @@
 package com.akjava.gwt.androidhtml5.client;
 
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.akjava.gwt.androidhtml5.client.data.ImageUrlData;
@@ -21,6 +22,12 @@ import com.akjava.gwt.lib.client.widget.cell.SimpleCellTable;
 import com.google.common.base.Optional;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Lists;
+import com.google.gwt.cell.client.ActionCell;
+import com.google.gwt.cell.client.ActionCell.Delegate;
+import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.CompositeCell;
+import com.google.gwt.cell.client.FieldUpdater;
+import com.google.gwt.cell.client.HasCell;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.ImageElement;
@@ -30,6 +37,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ErrorEvent;
 import com.google.gwt.text.shared.Renderer;
 import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
@@ -60,7 +68,7 @@ public class GifMaker extends Html5DemoEntryPoint {
 	private DockLayoutPanel dock;
 	private HorizontalPanel topPanel;
 	//EasyCellTableObjects<T>
-	private EasyCellTableObjects<ImageUrlData> EasyCellTableObjects;
+	private EasyCellTableObjects<ImageUrlData> easyCellTableObjects;
 	private Button makeBt;
 	private ValueListBox<Integer> speedBox;
 
@@ -135,11 +143,11 @@ public class GifMaker extends Html5DemoEntryPoint {
 			public void onClick(ClickEvent event) {
 				
 				
-				List<ImageElement> elements=FluentIterable.from(EasyCellTableObjects.getDatas()).transform(new DataToImageElement()).toList();
+				List<ImageElement> elements=FluentIterable.from(easyCellTableObjects.getDatas()).transform(new DataToImageElement()).toList();
 				
 				final String url=GifAnimeBuilder.from(elements).setQuality(qualityBox.getValue()).loop().delay(300).toDataUrl();
-				image.setVisible(true);
-				image.setUrl(url);
+				
+				setGif(url);
 				
 				//create buttons
 				downloadArea.clear();
@@ -149,12 +157,28 @@ public class GifMaker extends Html5DemoEntryPoint {
 				Button preview=new Button("Preview",new ClickHandler() {
 					@Override
 					public void onClick(ClickEvent event) {
-						image.setVisible(true);
-						image.setUrl(url);
-						//EasyCellTableObjects.getSelectionModel().u
+						setGif(url);
+						
 					}
+
+					
 				});
 				downloadArea.add(preview);
+				
+				Button stop=new Button("Stop",new ClickHandler() {
+					@Override
+					public void onClick(ClickEvent event) {
+						if(easyCellTableObjects.getDatas().size()>0){
+							//select first and it stop gif
+							easyCellTableObjects.setSelected(easyCellTableObjects.getDatas().get(0), true);
+						}else{
+							image.setVisible(false);//just hide
+						}
+						
+						image.setUrl(url);
+					}
+				});
+				downloadArea.add(stop);
 				
 			}
 		});
@@ -235,7 +259,7 @@ public class GifMaker extends Html5DemoEntryPoint {
 						@Override
 						public void update(int index, ImageUrlData object,
 								String value) {
-								EasyCellTableObjects.removeItem(object);
+							easyCellTableObjects.removeItem(object);
 						}
 						@Override
 						public String getValue(ImageUrlData object) {
@@ -279,6 +303,12 @@ public class GifMaker extends Html5DemoEntryPoint {
 					*/
 				
 					
+			
+				    
+				
+				    
+				   
+				    
 					
 				    TextColumn<ImageUrlData> fileInfoColumn = new TextColumn<ImageUrlData>() {
 					      public String getValue(ImageUrlData value) {
@@ -286,34 +316,45 @@ public class GifMaker extends Html5DemoEntryPoint {
 					    	  return value.getFileName();
 					      }
 					    };
-					    table.addColumn(fileInfoColumn,"Name");
+					   table.addColumn(fileInfoColumn,"Name");
 					    
 					    ButtonColumn<ImageUrlData> upBtColumn=new ButtonColumn<ImageUrlData>() {
 							@Override
 							public void update(int index, ImageUrlData object,
 									String value) {
 									
-									EasyCellTableObjects.upItem(object);
+								easyCellTableObjects.upItem(object);
 							}
 							@Override
 							public String getValue(ImageUrlData object) {
 								 return "Up";
 							}
 						};
-						table.addColumn(upBtColumn);
+						//table.addColumn(upBtColumn);
 						
 						 ButtonColumn<ImageUrlData> downBtColumn=new ButtonColumn<ImageUrlData>() {
 								@Override
 								public void update(int index, ImageUrlData object,
 										String value) {
-										EasyCellTableObjects.downItem(object);
+									easyCellTableObjects.downItem(object);
 								}
 								@Override
 								public String getValue(ImageUrlData object) {
 									 return "Down";
 								}
 							};
-							table.addColumn(downBtColumn);
+						//	table.addColumn(downBtColumn);
+							
+							 table.addColumn(new ActionCellGenerator<ImageUrlData>(){
+									@Override
+									public void executeAt(int index,ImageUrlData object) {
+										if(index==0){
+										easyCellTableObjects.upItem(object);
+										}else{
+											easyCellTableObjects.downItem(object);
+										}
+									}										
+									}.generateColumn(Lists.newArrayList("up","down")));
 					    
 					    
 			}
@@ -328,7 +369,7 @@ public class GifMaker extends Html5DemoEntryPoint {
 		
 		cellTable.setWidth("100%");
 		cellScroll.add(cellTable);
-		EasyCellTableObjects=new EasyCellTableObjects<ImageUrlData>(cellTable,false) {
+		easyCellTableObjects=new EasyCellTableObjects<ImageUrlData>(cellTable,false) {
 			@Override
 			public void onSelect(ImageUrlData selection) {
 				doSelect(selection);
@@ -364,7 +405,28 @@ public class GifMaker extends Html5DemoEntryPoint {
 	
 
 
-	
+	private class ActionHasCell implements HasCell<ImageUrlData, ImageUrlData> {
+	    private ActionCell<ImageUrlData> cell;
+
+	    public ActionHasCell(String text, Delegate<ImageUrlData> delegate) {
+	        cell = new ActionCell<ImageUrlData>(text, delegate);
+	    }
+
+	    @Override
+	    public Cell<ImageUrlData> getCell() {
+	        return cell;
+	    }
+
+	    @Override
+	    public FieldUpdater<ImageUrlData, ImageUrlData> getFieldUpdater() {
+	        return null;
+	    }
+
+	    @Override
+	    public ImageUrlData getValue(ImageUrlData object) {
+	        return object;
+	    }
+	}
 	
 
 
@@ -380,8 +442,22 @@ public class GifMaker extends Html5DemoEntryPoint {
 	 *
 	 */
 
-	
-	
+	private boolean ignoreUnselectImage;
+	private void setGif(final String url) {
+		ignoreUnselectImage=true;
+		easyCellTableObjects.unselect();
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			
+			@Override
+			public void execute() {
+				ignoreUnselectImage=false;
+				//called after once unselected
+				image.setVisible(true);
+				image.setUrl(url);
+			}
+		});
+		
+	}
 	
 
 	protected void loadFile(final File file,final String asStringText) {
@@ -397,14 +473,14 @@ public class GifMaker extends Html5DemoEntryPoint {
 				
 				final ImageUrlData data=new ImageUrlData(file.getFileName(),asStringText);
 				
-				EasyCellTableObjects.addItem(data);
+				easyCellTableObjects.addItem(data);
 				//updateList();
 				
 				//stack on mobile,maybe because of called async method
 				Scheduler.get().scheduleDeferred(new ScheduledCommand() {
 					@Override
 					public void execute() {
-						EasyCellTableObjects.setSelected(data, true);
+						easyCellTableObjects.setSelected(data, true);
 						makeBt.setEnabled(true);
 					}
 				});
@@ -453,8 +529,10 @@ public class GifMaker extends Html5DemoEntryPoint {
 	public void doSelect(ImageUrlData selection) {
 		this.selection=selection;
 		if(selection==null){
+			if(!ignoreUnselectImage){//when set gif no need do this
 			image.setUrl("");
 			image.setVisible(false);
+			}
 			
 		}else{
 			
